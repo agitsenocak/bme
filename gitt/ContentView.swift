4acc3d218cc11abf74fb8b32d23f1a4dd3aec53b
agit <senocakagit@gmail.com>
2023-11-23 11:25:56 +0300

    Initial Commit

---
+++ gitt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gitt
+//
+//  Created by AGİT on 23.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gitt/gittApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittApp.swift
+//  gitt
+//
+//  Created by AGİT on 23.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct gittApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
